82ad4fd8a04a92bd4b04cd21d7189fe57a1df879
Johan Alexis Estupiñan Zambrano <johan.zambrano2903@gmail.com>
2021-09-26 22:45:27 -0500

    Slides 4 y 5 del carrusel.Todavía falta arreglarlos

--- javascript/recursos.js
+++ javascript/recursos.js
@@ -10,7 +10,13 @@ let videos = ['https://www.youtube.com/embed/1j7AmQEYSFo',
  'https://www.youtube.com/embed/IbWOQWw1wkM',
  'https://www.youtube.com/embed/fzew3PgeCj0', 
  'https://www.youtube.com/embed/Y486eiRm1q8', 
- 'https://www.youtube.com/embed/ztrtZ0en8dw'];
+ 'https://www.youtube.com/embed/ztrtZ0en8dw',
+ 'https://www.youtube.com/embed/iYwI3hzaXTM', 
+ 'https://www.youtube.com/embed/f_Febo0yKhc', 
+ 'https://www.youtube.com/embed/Foy3PBmQKks',
+ 'https://www.youtube.com/embed/iGlGc057CLk', 
+ 'https://www.youtube.com/embed/MfsfseVRZOQ', 
+ 'https://www.youtube.com/embed/Q6MiwSun4L4'];
 
 function numRandom(){
 
@@ -27,6 +33,8 @@ function numRandom(){
     $("#slide1").html('<iframe class="d-block w-100" width="500" height="450" src="' + videos[numRandom()] + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     $("#slide2").html('<iframe class="d-block w-100" width="500" height="450" src="' + videos[numRandom() + 1] + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     $("#slide3").html('<iframe class="d-block w-100" width="500" height="450" src="' + videos[numRandom() - 1] + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+    $("#slide4").html('<iframe class="d-block w-100" width="500" height="450" src="' + videos[numRandom() + 2] + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+    $("#slide5").html('<iframe class="d-block w-100" width="500" height="450" src="' + videos[numRandom() - 2] + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     
     $("#video1").html('<iframe class="card-img-top" width="560" height="315" src="' + videos[Math.floor(Math.random()*videos.length)] + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     $("#video2").html('<iframe class="card-img-top" width="560" height="315" src="' + videos[Math.floor(Math.random()*videos.length)] + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
