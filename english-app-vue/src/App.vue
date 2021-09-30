8a9ab1f5adcec8652d4ada6ba2d971000b5ba099
diegohenao819 <diegohenao819@gmail.com>
2021-09-30 12:30:57 -0500

    vue 2.0

--- english-app-vue/src/App.vue
+++ english-app-vue/src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  
-    <helloWorld/>
-    <navbar/>
-    <primerComponente/>
+<div id="app">
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/login">Login</router-link>
+  </div>
+  <router-view/>
+  </div>
     
-    
   
+
+  
+    
   
 </template>
 
@@ -13,15 +19,16 @@
 <script>
 
 
-import Navbar from '@/components/Navbar.vue'
-import primerComponente from '@/components/miPrimerComponente'
+
+
+
 
 export default{
   name: 'App',
   components:{
+  
     
-    Navbar,
-    primerComponente
+ 
   }
 }
 </script>
